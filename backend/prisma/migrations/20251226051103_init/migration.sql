c1cb131b55d22cf1771a0d82d50f85bce7d60c8e
Doublepranks <sam.pantoja.ti@gmail.com>
2025-12-26 02:25:38 -0300

    feat: add initial migration script to create indexes for Influencer table

---
+++ backend/prisma/migrations/20251226051103_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Influencer_state_idx" ON "Influencer"("state");
+
+-- CreateIndex
+CREATE INDEX "Influencer_city_idx" ON "Influencer"("city");
+
+-- CreateIndex
+CREATE INDEX "Influencer_series_idx" ON "Influencer"("series");
